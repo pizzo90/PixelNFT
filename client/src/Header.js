c77a3305c41768c549a84a22e4cbc405c1feee61
grepdirect <grepdirect@gmail.com>
2021-06-09 18:22:32 +0200

    added

---
+++ client/src/Header.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+        <>
+            <nav>
+                <div className="logo"><span>PIXEL</span></div> 
+
+                <div className="main-menu">
+                    <a href="">
+                        <span>SHOP COLLECTION ON</span>
+
+                        <img src="https://lever-client-logos.s3.us-west-2.amazonaws.com/01451aa0-6bf3-468e-8caf-07c080094d9f-1614480724465.png"></img>
+                    </a>
+                </div>   
+             </nav>        
+                       
+
+
+
+
+
+    </>
+
+        )
+    }
+}
